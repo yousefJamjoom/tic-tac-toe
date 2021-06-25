9918a043f5da263a818a71c56925a1aee59518af
yousefjamjoom <yousefjmjoom@gmail.com>
2021-06-25 19:50:20 +0300

    Building the entire logic (Backend)

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,17 +1,72 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("DOM fully loaded and parsed");
- });
+let input;
+let turn;
+let header= document.createElement('h1');
 
- function handleGridClick(event) {
-    console.log(event);
+document.addEventListener("DOMContentLoaded", function (event) {
+    inputLoop();
+});
+
+function inputLoop() {
+    do {
+        input = prompt("Choose either x or o to start: ");
+        console.log(input);
+        if (input === "x") { turn = 1; } else if (input === "o") { turn = 0; }
+    } while (input != "x" && input != "o");
+}
+
+function handleGridClick(event) {
     let item = event.target;
-    console.log(item);
-    if (item != document.getElementsByClassName("grid-container")[0]){
-    item.textContent = 'X';
+    if (item != document.getElementsByClassName("grid-container")[0]) {
+        if (item.textContent != "x" && item.textContent != "o") {
+            if ((turn % 2) == 1) { item.textContent = "x"; }
+            else { item.textContent = "o"; }
+            turn += 1;
+            checkWinCondition();
+        }
+    }
 }
-}
-// className
 
-// alert('hello');
-// let item1 = document.getElementsByClassName("item-1")[0];
-// item1.textContent = "X";
+function checkWinCondition(){
+    checkDiagonals();
+    checkRows();
+    checkColumn();
+}
+
+function checkDiagonals(){
+    checkLine("0","4","8");
+    checkLine("2","4","6");
+}
+
+function checkRows() {
+    checkLine("0","1","2");
+    checkLine("3","4","5");
+    checkLine("6","7","8");
+}
+
+function checkColumn() {
+    checkLine("0","3","6");
+    checkLine("1","4","7");
+    checkLine("2","5","8");
+}
+
+function checkLine(x,y,z) {
+    if (document.getElementById(x).textContent === document.getElementById(y).textContent && document.getElementById(y).textContent === document.getElementById(z).textContent){ checkWhoWins(x); }
+}
+
+function checkWhoWins(x){
+    if ( document.getElementById(x).textContent === "x"){ declareWinner("X"); } 
+    else if( document.getElementById(x).textContent === "o"){ declareWinner("O"); }
+}
+
+function declareWinner(v){
+    header.innerText = v + ' has won';
+    document.body.appendChild(header);
+    resetTheGame()
+}
+
+function resetTheGame() {
+    for (item of document.getElementsByClassName('grid-item')) {
+        item.textContent = '';
+    }
+    inputLoop();
+}
